b9d2d034237a66a2f37f9f1a54226b713fecb5f3
santiago rojas rodríguez <santiagorojas772@gmail.com>
2026-07-03 19:21:10 -0500

    se creo el ejercicio de palindromos con metoos

---
+++ palindromosment.js
@@ -0,0 +1,17 @@
+//palindromo con metodos
+//palabra normal
+
+let w2 = ["M","a","c"];
+//se invierte lapalabra
+let w2b= w2.reverse();
+//se junta la palñabra invertida.
+let neww = w2b.join(' ')
+// si se leen iguales es palindromo
+if (w2 === w2b) {
+    console.log("es palindroma");
+   //en casoide que no se lean iguales no es palindromo 
+} else {
+    console.log("no es palindromo");
+    
+    
+}
