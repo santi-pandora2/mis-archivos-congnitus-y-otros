f42dd96fa9dcde8b6fc43820534df9f240866119
santiago rojas rodríguez <santiagorojas772@gmail.com>
2026-07-03 22:05:05 -0500

    se update archives

--- palindromosment.js
+++ palindromosment.js
@@ -7,7 +7,7 @@ let w2b= w2.reverse();
 //se junta la palñabra invertida.
 let neww = w2b.join(' ')
 // si se leen iguales es palindromo
-if (w2 === w2b) {
+if (w2 === neww) {
     console.log("es palindroma");
    //en casoide que no se lean iguales no es palindromo 
 } else {
